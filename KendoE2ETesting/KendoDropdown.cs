33a8be8ebf5fe1ad579244e4ef04ad6024ebacc2
Toby Wicks <toby.wicks@commercevision.com.au>
2024-07-08 12:37:08 +1000

    Working if run slowly

--- KendoE2ETesting/KendoDropdown.cs
+++ KendoE2ETesting/KendoDropdown.cs
@@ -15,6 +15,33 @@ public class KendoDropdown
             // document.querySelector('.k-dropdownlist')
 
             await page.GotoAsync("https://demos.telerik.com/kendo-ui/dropdownlist/index");
+            var downdown = page.Locator(".k-dropdownlist");
+
+            var dropdownButton = downdown.Locator("span.k-input-button");
+            await dropdownButton.ClickAsync();
+
+            // PROBLEM: This won't work if there are multiple dropdowns open simultaneously!
+            var dropdownList = page.Locator(".k-dropdownlist-popup");
+            
+            // https://playwright.dev/docs/other-locators
+            var berlin = dropdownList.Locator(@"li:has(span:has-text(""Berlin""))");
+            await berlin.ClickAsync(new LocatorClickOptions());
+
+            await Task.Delay(5000);
+        });
+
+    }
+
+    [Fact]
+    public async Task DropDownTest_WORKS()
+    {
+        await PlaywrightExtensions2.ForNewPage(async page =>
+        {
+            //https://demos.telerik.com/kendo-ui/dropdownlist/index
+            // span.k-input-button
+            // document.querySelector('.k-dropdownlist')
+
+            await page.GotoAsync("https://demos.telerik.com/kendo-ui/dropdownlist/index");
             var dropDownButton = page.Locator("span.k-input-button");
             await dropDownButton.ClickAsync();
 
